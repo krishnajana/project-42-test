e6290ffc2e865bd786e9f34a7839252167863f1c
krishnajana <76243764+krishnajana@users.noreply.github.com>
2021-05-29 15:17:46 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,19 @@
+class Drops{
+  constructor(x,y){
+    var options = {
+        friction: 0.1
+    }
+     this.rain = Bodies.circle(x,y,5,options)
+
+  }
+                                                                                                   
+
+
+
+ 
+
+
+
+
+
+}
